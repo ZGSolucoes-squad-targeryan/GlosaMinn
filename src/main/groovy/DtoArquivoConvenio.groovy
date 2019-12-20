9b499fcc54451116c3eeece66fbd9dc244ad0e10
douglas freitas <douglas.amorim@zgsolucoes.com.br>
2019-12-20 15:38:47 -0300

    Adicionando Receptor dos arquivos, Criando DtoArquivoConvenio, removendo testes criados automaticamente, e Criando teste para leitura do json

---
+++ src/main/groovy/DtoArquivoConvenio.groovy
@@ -0,0 +1,25 @@
+import groovy.transform.CompileStatic
+
+@CompileStatic
+class DtoArquivoConvenio {
+    String lote
+    String numeroGuiaPrestador
+    String numeroSolicitacaoInternacao
+    String numeroGuiaOperadora
+    String senha
+    String matricula
+    String RN
+    String nome
+    String dataExecucao
+    String horaInicial
+    String horaFinal
+    String codigoTabela
+    String codigoProcedimento
+    String descricaoProcedimento
+    String quantidadeExecutada
+    String valorUnitario
+    String valorApresentado
+    String valorPago
+    String valorGlosa
+    String motivoGlosa
+}
