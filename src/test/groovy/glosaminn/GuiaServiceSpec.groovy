b18a03bd725d4dc56ac5f9c540183b052ed8d3c2
gabriel bahia <>
2019-12-20 13:42:18 -0300

    Dominio convenio alterado e Hospital excluido

---
+++ src/test/groovy/glosaminn/GuiaServiceSpec.groovy
@@ -0,0 +1,18 @@
+package glosaminn
+
+import grails.testing.services.ServiceUnitTest
+import spock.lang.Specification
+
+class GuiaServiceSpec extends Specification implements ServiceUnitTest<GuiaService> {
+
+    def setup() {
+    }
+
+    def cleanup() {
+    }
+
+    void "test something"() {
+        expect: "fix me"
+        true == false
+    }
+}
